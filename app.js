4ae470bb8ca57c72e87c6ca1e9c72cdbf86c5b35
Jerónimo Gascón Sánchez <jergassan@alu.edu.gva.es>
2024-11-27 17:36:24 +0100

    Aitana - Estilos, Testeo, Aceptar letra Ñ Daniel - Testeo, aceptar letra Ñ Jero - Lógica de finalización del juego, Función que actualiza palabra en HTML

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Variables globales
 let words = []
 let imagenHtml = document.getElementsByTagName('img')[0]
 let palabraSecretaHtml = document.getElementById('palabra-secreta')
@@ -5,21 +6,24 @@ let palabraSecreta = ''
 let palabraOculta = ''
 let formularioHtml = document.getElementsByTagName('form')[0]
 let intentos = 0
+let letrasUsadas = []
 
-const leerJsonPalabras = async callback => {
+//Función que obtiene los datos del archivo JSON
+const leerJsonPalabras = async () => {
   const words = await fetch('./words.json')
   let wordsJson = await words.json()
   return wordsJson
 }
 
+//Función que obtiene las palabras del archivo JSON
 const obtenerPalabras = async () => {
   let jsonData = await leerJsonPalabras()
   const words = jsonData.words
 
   palabraSecreta = words[Math.floor(Math.random() * words.length)].item
-  console.log(palabraSecreta)
-  transformarPalabra()
 
+  console.log('PALABRA SECRETA:', palabraSecreta)
+  transformarPalabra()
   palabraSecretaHtml.innerText = palabraOculta
 }
 
@@ -39,9 +43,15 @@ function letraIntroducida(event) {
 
   const formChar = document.getElementsByTagName('input')[0].value.toLowerCase()
 
+  if (letrasUsadas.includes(formChar)) return
+
   if (esSoloLetra(formChar)) {
-    console.log('Es una letra válida.')
     // Sustituir los guiones por la letra introducida en la palabra mostrada en pantalla
+    actualizarPalabra(palabraSecretaHtml.textContent, formChar, palabraSecreta)
+
+    // Añadir la letra introducida a la lista de letras usadas
+    letrasUsadas.push(formChar)
+
     // Cambiar el estilo de la tecla en pantalla correspondiente a la letra introducida
     document
       .getElementById(formChar)
@@ -50,14 +60,56 @@ function letraIntroducida(event) {
           ? 'correct'
           : 'incorrect'
       )
+
+    palabraSecreta.toLowerCase().includes(formChar) ? null : intentos++
+    cambiarImagen(intentos)
+
+    if (
+      palabraSecretaHtml.textContent.toLowerCase() ===
+      palabraSecreta.toLowerCase()
+    ) {
+      // Mostrar elemento de victoria
+      document.getElementsByClassName('win')[0].style.display = 'block'
+
+      // Ocultar formulario con el Tag Name
+      document.getElementsByTagName('form')[0].style.display = 'none'
+    }
+
+    if (intentos >= 8) {
+      // Mostrar elemento de derrota
+      document.getElementsByClassName('lose')[0].style.display = 'block'
+
+      // Ocultar formulario con el Tag Name
+      document.getElementsByTagName('form')[0].style.display = 'none'
+    }
   } else {
-    console.log('No es una letra válida.')
+    console.log('No es una letra válida.') // Mostrar mensaje en la consola
   }
+}
+
+//Función para actualizar la palabra en el HTML
+function actualizarPalabra(palabraConGuiones, letra, palabraSecreta) {
+  // Convertir palabraConGuiones a array
+  let palabraConGuionesArray = palabraConGuiones.split('')
+
+  // Reemplazar guiones por la letra correspondiente si es correcta
+  for (let i = 0; i < palabraSecreta.length; i++) {
+    if (palabraSecreta[i].toLowerCase() === letra.toLowerCase()) {
+      palabraConGuionesArray[i] = letra // Actualizar la posición correspondiente
+    }
+  }
+
+  // Convertir array de nuevo a un string
+  palabraConGuiones = palabraConGuionesArray.join('')
+
+  // Actualizar HTML con el nuevo texto
+  document.getElementById('palabra-secreta').textContent =
+    palabraConGuiones.toUpperCase()
 }
 
 // Comprobar que el input del formulario es sólo una letra
 function esSoloLetra(input) {
-  const regex = /^[a-zA-Z]$/
+  const regex = /^[A-Za-zÑñ]+$/
 
   return regex.test(input)
 }
@@ -65,10 +117,4 @@ function esSoloLetra(input) {
 // Modificar SRC de la imagen para mostrar la imagen correspondiente al intento actual
 function cambiarImagen(intento) {
   imagenHtml.src = './images/Hangman-' + intento + '.png'
-}
-
-while (intentos < 8) {
-  console.log(intentos)
-  cambiarImagen(intentos)
-  intentos++
 }
